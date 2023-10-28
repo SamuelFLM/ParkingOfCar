26c3806f7b61300fbd175a0df4886639ce719526
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-10-28 06:06:05 -0300

    add

---
+++ Src/Parking.API/Controllers/ParkingController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Parking.API.Controllers
+{
+    [ApiController]
+    [Route("v1/parking-of-car")]
+    public class ParkingController : ControllerBase
+    {
+        
+    }
+}
